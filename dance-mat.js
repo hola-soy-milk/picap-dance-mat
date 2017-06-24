66b1dd21b28ef075ad11b11c7693e3468e8b1a6b
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2017-06-24 15:02:37 +0200

    Clean up code

--- dance-mat.js
+++ dance-mat.js
@@ -14,60 +14,70 @@ var p2Down = 11;
 
 var p1Start = 12;
 var p1Back = 13;
+
+parsePressedKeys = function(data) {
+  var pressedKeys = [];
+  data.forEach(function(electrode, i) {
+    if (electrode.isNewTouch) {
+      switch(i) {
+        case 0:
+          pressedKeys.push(p1Left);
+          break;
+        case 1:
+          pressedKeys.push(p1Right);
+          break;
+        case 2:
+          pressedKeys.push(p1Up);
+          break;
+        case 3:
+          pressedKeys.push(p1Down);
+          break;
+        case 4:
+          pressedKeys.push(p2Left);
+          break;
+        case 5:
+          pressedKeys.push(p2Right);
+          break;
+        case 6:
+          pressedKeys.push(p2Up);
+          break;
+        case 7:
+          pressedKeys.push(p2Down);
+          break;
+        case 8:
+          pressedKeys.push(p2Up);
+          break;
+        case 9:
+          pressedKeys.push(p1Start);
+          break;
+        case 10:
+          pressedKeys.push(p1Back);
+          break;
+      }
+    }
+  });
+  return pressedKeys;
+}
+
+keystrokeFromPressedKeys = function(pressedKeys) {
+  keystroke = "\\0\\0";
+  pressedKeys.forEach(function(key) {
+    keystroke += "\\" + key;
+  });
+  while(keystroke.length < 16) {
+    keystroke += "\\0";
+  }
+  return keystroke.substring(0, 16);
+}
+
 // correct address for the Pi Cap - other boards may vary
 mpr121 = new MPR121('0x5C');
 mpr121.setTouchThreshold(40);
 mpr121.setReleaseThreshold(20);
 
 mpr121.on('data', function(data) {
-  var keys = [];
-  data.forEach(function(electrode, i) {
-    if (electrode.isNewTouch) {
-      switch(i) {
-        case 0:
-          keys.push(p1Left);
-          break;
-        case 1:
-          keys.push(p1Right);
-          break;
-        case 2:
-          keys.push(p1Up);
-          break;
-        case 3:
-          keys.push(p1Down);
-          break;
-        case 4:
-          keys.push(p2Left);
-          break;
-        case 5:
-          keys.push(p2Right);
-          break;
-        case 6:
-          keys.push(p2Up);
-          break;
-        case 7:
-          keys.push(p2Down);
-          break;
-        case 8:
-          keys.push(p2Up);
-          break;
-        case 9:
-          keys.push(p1Start);
-          break;
-        case 10:
-          keys.push(p1Back);
-          break;
-      }
-    }
-  });
-  keystroke = "\\0\\0";
-  keys.forEach(function(key) {
-    keystroke += "\\" + key;
-  });
-  while(keystroke.length < 16) {
-    keystroke += "\\0";
-  }
-  keystroke = keystroke.substring(0, 16);
+  keys = parsePressedKeys(data);
+  keystroke = keystrokeFromPressedKeys(keys);
   console.log(spawn.execSync('echo -ne "' + keystroke + '" > /dev/hidg0').toString());
 });
 
